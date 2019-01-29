9fbaad2ecf943db92119dd04a0b3b102af37004e
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2019-01-29 12:29:18 +0800

    Added strategy pattern

--- StrategyPattern/StrategyPattern.cpp
+++ StrategyPattern/StrategyPattern.cpp
@@ -1,21 +1,136 @@
-// StrategyPattern.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Strategy;
+
+class TestBed
+{
+public:
+	enum StrategyType
+	{
+		Dummy, Left, Right, Center
+	};
+	TestBed()
+	{
+		strategy_ = NULL;
+	}
+	void setStrategy(int type, int width);
+	void doIt();
+private:
+	Strategy *strategy_;
+};
+
+class Strategy
+{
+public:
+	Strategy(int width) : width_(width) {}
+	void format()
+	{
+		char line[80], word[30];
+		ifstream inFile("quote.txt", ios::in);
+		line[0] = '\0';
+
+		inFile >> word;
+		strcat(line, word);
+		while (inFile >> word)
+		{
+			if (strlen(line) + strlen(word) + 1 > width_)
+				justify(line);
+			else
+				strcat(line, " ");
+			strcat(line, word);
+		}
+		justify(line);
+	}
+protected:
+	int width_;
+private:
+	virtual void justify(char *line) = 0;
+};
+
+class LeftStrategy : public Strategy
+{
+public:
+	LeftStrategy(int width) : Strategy(width) {}
+private:
+	/* virtual */void justify(char *line)
+	{
+		cout << line << endl;
+		line[0] = '\0';
+	}
+};
+
+class RightStrategy : public Strategy
+{
+public:
+	RightStrategy(int width) : Strategy(width) {}
+private:
+	/* virtual */void justify(char *line)
+	{
+		char buf[80];
+		int offset = width_ - strlen(line);
+		memset(buf, ' ', 80);
+		strcpy(&(buf[offset]), line);
+		cout << buf << endl;
+		line[0] = '\0';
+	}
+};
+
+class CenterStrategy : public Strategy
+{
+public:
+	CenterStrategy(int width) : Strategy(width) {}
+private:
+	/* virtual */void justify(char *line)
+	{
+		char buf[80];
+		int offset = (width_ - strlen(line)) / 2;
+		memset(buf, ' ', 80);
+		strcpy(&(buf[offset]), line);
+		cout << buf << endl;
+		line[0] = '\0';
+	}
+};
+
+void TestBed::setStrategy(int type, int width)
+{
+	delete strategy_;
+	if (type == Left)
+		strategy_ = new LeftStrategy(width);
+	else if (type == Right)
+		strategy_ = new RightStrategy(width);
+	else if (type == Center)
+		strategy_ = new CenterStrategy(width);
+}
+
+void TestBed::doIt()
+{
+	strategy_->format();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	TestBed test;
+	int answer, width = 0;
+
+	cout << "Exit(0) Left(1) Right(2) Center(3): ";
+	cin >> answer;
+
+	while (answer)
+	{
+		while (width <= 0 || width >= 80)
+		{
+			cout << "Width (1-79): ";
+			cin >> width;
+		}
+		test.setStrategy(answer, width);
+		test.doIt();
+		cout << "Exit(0) Left(1) Right(2) Center(3): ";
+		cin >> answer;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
