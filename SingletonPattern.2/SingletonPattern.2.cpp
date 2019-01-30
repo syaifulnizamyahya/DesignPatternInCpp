3793227e82d854a0ec119b44fd909edec12cbadb
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2019-01-30 10:13:45 +0800

    Simplify singleton

--- SingletonPattern.2/SingletonPattern.2.cpp
+++ SingletonPattern.2/SingletonPattern.2.cpp
@@ -9,43 +9,28 @@ using namespace std;
 // 3. definition
 class GlobalClass
 {
-private:
-	int m_value;
-
 public:
-	GlobalClass(int v = 0)
+	GlobalClass()
 	{
-		m_value = v;
+		value = 100;
 	}
 
 	int get_value()
 	{
-		return m_value;
+		return value;
 	}
+
 	void set_value(int v)
 	{
-		m_value = v;
+		value = v;
 	}
+
+private:
+	int value;
 };
 
 // 1. declaration
-GlobalClass *global_ptr = 0;
-
-void foo(void)
-{
-	if (!global_ptr)
-		global_ptr = new GlobalClass;
-	global_ptr->set_value(1);
-	cout << "foo: global_ptr is " << global_ptr->get_value() << '\n';
-}
-
-void bar(void)
-{
-	if (!global_ptr)
-		global_ptr = new GlobalClass;
-	global_ptr->set_value(2);
-	cout << "bar: global_ptr is " << global_ptr->get_value() << '\n';
-}
+GlobalClass *global_ptr;
 
 int main()
 {
@@ -53,8 +38,8 @@ int main()
 	if (!global_ptr)
 		global_ptr = new GlobalClass;
 	cout << "main: global_ptr is " << global_ptr->get_value() << '\n';
-	foo();
-	bar();
+	global_ptr->set_value(200);
+	cout << "main: global_ptr is " << global_ptr->get_value() << '\n';
 
 	cin.get();
 }
